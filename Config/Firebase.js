0c768cffc2ea17065ad8fb2573da802443a36ca8
s-ashwin <ashwin.sivanagarajan@gmail.com>
2020-08-06 16:14:11 +0530

    firebase config

---
+++ Config/Firebase.js
@@ -0,0 +1,10 @@
+export default {
+    apiKey: "AIzaSyC0_oIAV5iDLrR5Gv8AZaqK-LftGhCz6SU",
+    authDomain: "github-app-66d9b.firebaseapp.com",
+    databaseURL: "https://github-app-66d9b.firebaseio.com",
+    projectId: "github-app-66d9b",
+    storageBucket: "github-app-66d9b.appspot.com",
+    messagingSenderId: "676667787391",
+    appId: "1:676667787391:web:321d8b08c4a43675fc174a",
+    measurementId: "G-60XHTK4JS3"
+  };
