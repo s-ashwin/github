6ee20f3718c47aa30f965c26705644a83bce0e7a
s-ashwin <ashwin.sivanagarajan@gmail.com>
2020-08-11 17:13:29 +0530

    Created 404 page

--- src/Pages/Notfound.js
+++ src/Pages/Notfound.js
@@ -1,8 +1,15 @@
 import React from "react";
+import { Container } from "reactstrap";
+import { DiGithubBadge} from "react-icons/di";
 
 const Notfound = ()=>{
     return(
-        <h1>Notfound</h1>
+        <Container className="text-center">
+            <p style={{fontSize:100, fontWeight:'100'}} className='text-dark'>404</p>
+            <p style={{ fontWeight:'900'}} className='text-dark'>File not found</p>
+            <p style={{ fontWeight:'100'}} className='text-dark'>The site configured at this address does not contain the requested file.</p>
+            <DiGithubBadge size={45} className='text-dark' ></DiGithubBadge>
+        </Container>
     )
 }
 export default Notfound;
