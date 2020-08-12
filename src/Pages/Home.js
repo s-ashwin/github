4cb1b68d15effbc34546494469805925b28a5f97
s-ashwin <ashwin.sivanagarajan@gmail.com>
2020-08-12 19:38:03 +0530

    done with homepage

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -6,7 +6,7 @@ import { Context } from "../Context/Context";
 import { toast } from "react-toastify";
 import Usercard from "../Components/Usercard";
 import Repo from "../Components/Repo";
-import { GoRepo} from "react-icons/go";
+import { GoRepo, GoLogoGithub} from "react-icons/go";
 
 
 const Home = ()=>{
@@ -26,7 +26,29 @@ const Home = ()=>{
     }
     if(!context.user?.uid){
         return(
-            <div>No user</div>
+            <div className="defaulthome">
+                <div className="hero">
+                    <Container>
+                        <Row>
+                            <Col md='6' className="intro">
+                                <h1>Built for developers</h1>
+                                <p>GitHub is a development platform inspired by the way you work. From open source to business, you can host and review code, manage projects, and build software alongside 50 million developers.</p>
+                            </Col>
+                            <Col md='6' className="auth">
+                                <GoLogoGithub size={100}></GoLogoGithub>
+                            <div className=""> <p>If you are an existing user</p> <Button onClick={event =>  window.location.href='/signin'} className="my-2 btn-success">Sign In</Button> <p>Or, Create a New Account</p> <Button onClick={event =>  window.location.href='/signup'} className="my-2 btn-success"> Sign Up</Button>  </div>
+                            </Col>
+                        </Row>
+                    </Container>
+                </div>
+                <div className="herotwo">
+                    <Container>
+                        <h1>GitHub Clone App</h1>
+                        <p>Create an account, Search your friends on GitHub</p>
+                        <p>(As of now, this app allows you only to search for profiles on github and view their repositories)</p>
+                    </Container>
+                </div>
+            </div>
         )
     }
     else{
