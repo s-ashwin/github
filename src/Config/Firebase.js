1be6a65742a5c20cb2f2ad0d3a745d971c2a0136
s-ashwin <ashwin.sivanagarajan@gmail.com>
2020-08-21 13:29:05 +0530

    updates

--- src/Config/Firebase.js
+++ src/Config/Firebase.js
@@ -1,10 +1,9 @@
 export default {
     apiKey: process.env.FIREBASE_API_KEY,
-    authDomain: "github-app-66d9b.firebaseapp.com",
-    databaseURL: "https://github-app-66d9b.firebaseio.com",
-    projectId: "github-app-66d9b",
-    storageBucket: "github-app-66d9b.appspot.com",
-    messagingSenderId: "676667787391",
-    appId: "1:676667787391:web:321d8b08c4a43675fc174a",
-    measurementId: "G-60XHTK4JS3"
+    authDomain: process.env.AUTH_DOMAIN,
+    databaseURL: process.env.DB_URL,
+    projectId: process.env.PROJECT_ID,
+    storageBucket: process.env.STORE_BUCKET,
+    messagingSenderId: process.env.SENDER_ID,
+    appId: process.env.API_ID,
   };
